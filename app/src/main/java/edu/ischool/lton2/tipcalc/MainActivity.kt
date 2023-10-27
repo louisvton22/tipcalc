17d9287476865b8f505748298c89c0360833a7a2
louisvton22 <81780806+louisvton22@users.noreply.github.com>
2023-10-26 20:33:58 -0700

    fixing pennies

--- app/src/main/java/edu/ischool/lton2/tipcalc/MainActivity.kt
+++ app/src/main/java/edu/ischool/lton2/tipcalc/MainActivity.kt
@@ -7,26 +7,37 @@ import android.text.TextWatcher
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.text.InputFilter
 import android.widget.EditText
 import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
+    var amount: Int? = 0;
+    var isFormatting = false;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var amount: Int = 0;
+
         var textInput: EditText = findViewById(R.id.editTextNumber)
         var tipButton: Button = findViewById(R.id.button)
 
-        textInput.addTextChangedListener(object: TextWatcher {
-            override fun afterTextChanged(s: Editable?) {}
 
+        textInput.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                amount = convertToPennies(s.toString())
+                if (!isFormatting) {
+                    isFormatting = true
+                    if (s.toString().startsWith("$")) {
+
+                    }
+                    textInput.setText("$ ${s.toString()}")
+                    isFormatting = false
+                }
+            }
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                amount += 1
-                Log.i("Tip Calculator", "Amount inputted changed to : $s")
             }
         })
 
@@ -35,13 +46,17 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun convertToPennies(s:String?): String?  {
-        return ""
+    fun convertToPennies(s:String?): Int?  {
+        Log.i("Tip Calculator", "converting $s to pennies")
+        return s?.toDouble()?.times(100)?.toInt()
 
     }
 
+    fun convertToDollars() {}
+
     fun calculateTip(s:String?):String {
         // convert to integer then calculate 15% of that
-        return 0.toString()
+        val tip = amount * .15
+        return "$ ${tip.toString()}"
     }
 }
